ba4d1d7bb7767602f02fce152b3e5fb5a5cad201
kajal764 <kajaldw666@gmail.com>
2020-10-13 00:29:46 +0530

    Added: Configuration for common properties

---
+++ cloud-config-server/src/test/java/com/fudoo/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fudoo.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
